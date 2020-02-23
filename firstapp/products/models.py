e473d909ebcd86d98e7ae1c3fdc03e346beb1dcd
hghimanshu <hghimanshu81@gmail.com>
2020-02-23 18:17:23 +0530

    changed the fields of models

--- firstapp/products/models.py
+++ firstapp/products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Product(models.Model):
-    title = models.TextField()
-    price = models.TextField()
-    description = models.TextField()
+    title = models.CharField(max_length=120)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    description = models.TextField(blank=True, null=True)
