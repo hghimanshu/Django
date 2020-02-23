4d373b850df08f269cf80645e9feb08be4a41224
hghimanshu <hghimanshu81@gmail.com>
2020-02-23 19:07:14 +0530

    added the request

--- firstapp/pages/views.py
+++ firstapp/pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
     return HttpResponse("<h2>This is first route </h2>")
