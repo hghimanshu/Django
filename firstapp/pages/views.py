15a6d2167e87a2fba45eeb14623e752ec5adc145
hghimanshu <hghimanshu81@gmail.com>
2020-10-05 23:25:30 +0530

    new templates and views

--- firstapp/pages/views.py
+++ firstapp/pages/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello</>")
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
