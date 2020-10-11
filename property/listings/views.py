d584c0ddfd5a7563308cd9c3ae10e2ffabc1c538
hghimanshu <hghimanshu81@gmail.com>
2020-10-11 23:42:39 +0530

    now working on searching the query from the db

--- property/listings/views.py
+++ property/listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
+from .choices import price_choices, state_choices, bedroom_choices
 
 # Create your views here.
 def index(request):
@@ -19,4 +20,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', {})
 
 def search(request):
-    return render(request, 'listings/search.html', {})
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
