2776d6678cc9575a06a57a620bdc667cea764860
hghimanshu <hghimanshu81@gmail.com>
2020-10-12 00:03:00 +0530

    updated the search query sets

--- property/listings/views.py
+++ property/listings/views.py
@@ -20,10 +20,38 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', {})
 
 def search(request):
+    query_set = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_set = query_set.filter(description__icontains=keywords) 
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_set = query_set.filter(city__iexact=city) 
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_set = query_set.filter(state__iexact=state) 
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_set = query_set.filter(bedrooms__lte=bedrooms) 
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_set = query_set.filter(price__lte=price) 
+
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'listings': query_set
     }
 
     return render(request, 'listings/search.html', context)
