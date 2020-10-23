f1c65be8084c3122824ffbbe858b15153a2bff4d
hghimanshu <hghimanshu81@gmail.com>
2020-10-23 23:55:34 +0530

    working on views

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+    
