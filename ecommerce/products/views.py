164bb1533cb03a422f6e1fb7a16f0b5afad3a827
hghimanshu <hghimanshu81@gmail.com>
2020-10-24 00:23:36 +0530

    deails view for each product

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Product
 
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+    
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/details.html"
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
-    
