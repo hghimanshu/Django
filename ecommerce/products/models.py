b017866f4c20712ea79bb40028dde3e940d0a9d0
hghimanshu <hghimanshu81@gmail.com>
2020-10-25 01:03:40 +0530

    custom querysets and model managers

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import os
 import uuid
 import random
+from django.db.models.signals import pre_save
+from .utils import unique_slug_generator
 
 def get_filename_ext(filename):
     base_name = os.path.basename(filename)
@@ -26,11 +28,45 @@ def upload_image_path(instance, filename):
         final_filename=final_filename
     )
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def features(self):
+        return self.get_queryset().featured()
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, default='abc', unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=0.0)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
+    objects = ProductManager()
+    
     def __str__(self):
         return self.title
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
